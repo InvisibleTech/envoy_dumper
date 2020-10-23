8fb4e7152bff93753d4f8ce1a6ddf29647f2abf2
John Ferguson <john.ferguson@kognition.ai>
2020-10-23 15:12:15 -0400

    Adding endpoint.

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { middleware, errorMiddleware } = require('@envoy/envoy-integrations-sdk');
+
+const app = express();
+app.use(middleware());
+
+app.post('/dump_payload', async () => {
+    const envoy = req.envoy; // our middleware adds an "envoy" object to req.
+    console.log(">>>>>>>>>>>>>>>>  Envoy :");
+    console.log(JSON.stringify(envoy));
+    console.log(">>>>>>>>>>>>>>>>  Envoy payload:");
+    console.log(JSON.stringify(envoy.payload));
+    console.log(">>>>>>>>>>>>>>>>  Envoy meta ");
+    console.log(JSON.stringify(envoy.meta));
+
+    res.send({"msg": "Dumped"});
+});
+
+app.use(errorMiddleware());
+
+const listener = app.listen(process.env.PORT || 0, () => {
+  console.log(`Listening on port ${listener.address().port}`);
+});
